b3a1fb0b78d9229a372dec6ca851bccf18f9cdbb
Robert <robert.sebastian.visu@gmail.com>
2026-05-11 20:56:45 +0200

    chore: inicializar proyecto dashboard Vue 3 con PrimeVue, Tailwind y unplugin-icons

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
+import Icons from 'unplugin-icons/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+    tailwindcss(),
+    Icons({ compiler: 'vue3', autoInstall: true }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
