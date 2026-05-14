66e83a79723f1569ea9f4bdd900c3f0c16d998b2
Robert Sebastian Visu <robert.sebastian.visu@gmail.com>
2026-05-14 15:44:19 +0200

    feat: ocultar el sidebar en la pantalla de login

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
+import { useRoute } from "vue-router";
 import AppSidebar from "@/components/app/AppSidebar.vue";
+
+const route = useRoute();
 </script>
 
 <template>
   <div class="flex h-screen bg-gray-50">
-    <AppSidebar />
+    <AppSidebar v-if="route.name !== 'login'" />
     <main class="flex-1 flex flex-col overflow-hidden p-8">
       <RouterView />
     </main>
